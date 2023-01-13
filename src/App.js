b4de5b2f72a68625dd59bd115e8cde9f2a941a6e
derekaoo <109321952+derekaoo@users.noreply.github.com>
2023-01-13 15:03:05 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Mi practica en codesandbox</h1>
+      <h2>Torres Olivas Derek Omar</h2>
+      <h2>Universidad Tecnologica de durango</h2>
+    </div>
+  );
+}
